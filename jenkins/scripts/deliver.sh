7ca680e1e8320a6edff3c40e3e4a533eae8da589
root <root@ip-172-31-80-175.ec2.internal>
2022-12-17 17:28:52 +0000

    comitted some data

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -24,3 +24,17 @@ echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
 java -jar target/${NAME}-${VERSION}.jar
+
+
+
+
+
+
+
+
+
+############################
+#yfghghdf
+#dshsdgyifdghif
+#dhfdgfd
+#
